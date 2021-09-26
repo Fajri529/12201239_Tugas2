37899ca99537e03c1e1ccfaf626ceef83b95cba4
Miftahul Fajri Sidqi <miftahulfajrisidqi529@gmail.com>
2021-09-26 15:46:27 +0700

    first commit

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "Assalamualikum, "; 
+        echo "Perkenalkan<br/>";
+        echo "Nama : Miftahul Fajri Sidqi<br/>". PHP_EOL;
+        echo "Nim : 12201239<br/>". PHP_EOL;
+        echo "Class : 12.3A.13<br/>". PHP_EOL;
+        echo "Alamat : Tasikmalaya,Kec.Culamega,Desa.Cintabodas,KP.PicungPugur<br/>". PHP_EOL;
+        echo "No HP : 085295665797<br/>". PHP_EOL;
+    }
+}
